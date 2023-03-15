643b6f76a3869d7dcd74ecc282813f5cc1584fbe
gunakarokenneth <75356345+gunakarokenneth@users.noreply.github.com>
2023-03-15 09:38:56 +0700

    update global

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         'graybg-sr':'#F6F6F6',
         'black-sr':'#303030',
         'graybar-sr':'#808080',
-        'blue-sr':'#003D73'
+        'blue-sr':'#003D73',
+        'blacktext-sr':'#303030'
       },
       margin:{
         '450px':'450px'
@@ -17,9 +18,27 @@ module.exports = {
       width:{
         '600px':'600px'
       },
+      width2:{
+        '1000px':'1000px'
+      },
+      width3:{
+        '530px':'530px'
+      },
+      width4:{
+        '188px':'188px'
+      },
       height:{
         '300px':'300px'
-      }
+      },
+      padding:{
+        '35px':'35px'
+      },
+      padding2:{
+        '80px':'80px'
+      },
+      rounded:{
+        '10px':'10px'
+      },
     },
   },
   plugins: [],
